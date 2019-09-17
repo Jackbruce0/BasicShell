6188ddca6fc47298b4ddec7b83a2dc019758d7fa
Jack Bruce <jackbruce0@gmail.com>
2019-09-17 10:06:05 -0700

    Handles basic commands
    Solved waiting problem for child processes

--- p2.c
+++ p2.c
@@ -16,10 +16,9 @@ char words[MAXITEM][STORAGE]; /* words collected from stdin */
 
 int main()
 {
-
-    char prompt[5] = "%1% ";
-    int wordcount = 0;
     char *newargv[20];
+    char prompt[5] = "%1% ";
+    int wordcount = 0, execstatus = 0, kidpid;
 
     //any necessary set-up, including signal catcher and setpgid();
     //check for the presense of argv[1]; redirect p2's input if appropriate
@@ -30,36 +29,36 @@ int main()
         //call parse function, setting [global] flags as needed;
         wordcount = parse(words);
         //if (getword() returned -1 and line is empty) break
-        
+        if (wordcount == -1) break;
+        if (wordcount == 0) continue; // reissue prompt if line is empty        
         // parse check
         int i;
         for(i = 0; i < wordcount; i++)
         {
-            printf("word %d: [%s]\n", i ,words[i]);
+        //    printf("word %d: [%s]\n", i ,words[i]);
             newargv[i] = words[i];
         }
-        if (wordcount == -1) break;
+        newargv[wordcount] = NULL;
 
-        execvp(newargv[0] , newargv);
-        //if (line is empty) continue;
         //handle builtins (done, cd, !!) and continue, or:
         //set up for redirection
-//        if (fork() == 0) {
+        if ((kidpid = fork()) == 0) {
         //  redirect I?O as requested (background children sometimes need
         //  their stdin redirected to /dev/null);
         //  use execvp() to start requested process;
-        //    execvp(newargv[0] , newargv);
-        //    if (execvp("/bin/cat" , "./p2.c") == -1)
-        //    {
-        //        perror("exec failed");
-        //        exit(9);
-        //    }
+            execstatus = execvp(newargv[0] , newargv);
+            if (execstatus == -1)
+            {
+                perror("exec failed");
+                exit(9);
+            }
         //  if the execvp() failed {
         //      print an error message;
         //      exit(9); [choose different exit values for different errors]
         //  }
-//        }
+        }
         //if appropriate, wait for child to complete;
+        wait(NULL);
         //else print the chidl's pid (and in this casek the child should
         //redirect its stdin to /dev/null [unless '<' specifies a better target]
     }
@@ -85,13 +84,13 @@ int parse(char words[][STORAGE])
     int wordcount = 0; // total number of words read
     for(;;)
     {
-            //(void) printf("n=%d, s=[%s]\n", c = getword(s), s);
-            c = getword(s);
-            strcpy(words[wordcount], s);
             
-            if (wordcount != 0 && c <= 0) break;
-            if (wordcount == 0 && c == -1) return -1;
-            wordcount++;
+        //(void) printf("n=%d, s=[%s]\n", c = getword(s), s);
+        c = getword(s);
+        strcpy(words[wordcount], s);
+        if (c == 0) break;
+        if (wordcount == 0 && c == -1) return -1;
+        wordcount++;
     }
     return wordcount;
 }
--- p2.h
+++ p2.h
@@ -10,6 +10,10 @@
  *****************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "getword.h"
 #define MAXITEM 100 /* max number of words per line */
 
